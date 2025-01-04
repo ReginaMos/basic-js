d2f98177e23756f88f1835dd9cb51e1ace7c34ab
Regina Moiseeva <moiseevaryu@gmail.com>
2025-01-04 14:07:58 +0500

    feat: add solution to vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value) {
+    if (value === false) {
+      this.flag = false;
+      return;
+    }
+    this.flag = true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!');
+  
+    const givingMessage = message.toUpperCase();
+    const givingKey = key.toUpperCase();
+    let encryptedMessage = '';
+
+    for(let i = 0, j = 0; i < givingMessage.length; i += 1, j += 1) {
+      if (givingMessage[i] >= 'A' && givingMessage[i] <= 'Z') {
+        const keyChar = givingKey[j % key.length];
+        const shift = ((givingMessage[i].charCodeAt() + keyChar.charCodeAt()) % 26) + 'A'.charCodeAt();
+
+        encryptedMessage += String.fromCharCode(shift);
+      } else {
+        encryptedMessage += givingMessage[i];
+        j -= 1;
+      }
+    }
+
+    return this.flag ? encryptedMessage : encryptedMessage.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) throw new Error('Incorrect arguments!');
+    
+    const givingMessage = encryptedMessage.toUpperCase();
+    const givingKey = key.toUpperCase();
+    let decryptedMessage = '';
+
+    for(let i = 0, j = 0; i < givingMessage.length; i += 1, j += 1) {
+      if (givingMessage[i] >= 'A' && givingMessage[i] <= 'Z') {
+        const keyChar = givingKey[j % key.length];
+        const shift = (givingMessage[i].charCodeAt() - keyChar.charCodeAt() + 26) % 26 + 'A'.charCodeAt();
+
+        decryptedMessage += String.fromCharCode(shift);
+      } else {
+        decryptedMessage += givingMessage[i];
+        j -= 1;
+      }
+    }
+
+    return this.flag ? decryptedMessage : decryptedMessage.split('').reverse().join('');
   }
 }
 
